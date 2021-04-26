2c40d4c6fe663edc963bcb5603fd8c40916151fa
Neto Silva <netosilv44445@gmail.com>
2021-04-26 20:53:08 -0300

    LoopStudios - Mobile First Version

---
+++ javascript/menu.js
@@ -0,0 +1,11 @@
+let menuHamburguer = document.querySelector('.hamburguerMenu')
+let menuList = document.querySelector('.menuList')
+menuHamburguer.addEventListener('click', function() {
+    if(menuList.style.display == '') {
+        menuList.style.display = 'block';
+        document.body.style.overflowY = 'hidden'
+    }else {
+        menuList.style.display = '';
+        document.body.style.overflowY = 'scroll'
+    }
+})
